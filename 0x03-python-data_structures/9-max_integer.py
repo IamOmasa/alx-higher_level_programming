23e8273445d653031a4bc97414f23eb660876af6
Omary Ally <omaryally.em@gmail.com>
2024-03-19 12:22:31 -0700

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a function that finds the biggest integer of a list"""
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+
+    max_value = my_list[0]
+
+    for num in my_list[1:]:
+        if num > max_value:
+            max_value = num
+
+    return (max_value)
