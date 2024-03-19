5377d7428cd60a841e3a55d3ffb80774bd3c9e58
Omary Ally <omaryally.em@gmail.com>
2024-03-19 12:58:17 -0700

    delete

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a function that deletes the item at a specific position in a list"""
+
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+
+    new_list = my_list[:idx]
+
+    new_list.extend(my_list[idx + 1:])
+
+    return new_list
