6f06555fefbbe90e1db393fc184dcae979dd4918
Omary Ally <omaryally.em@gmail.com>
2024-03-19 12:41:02 -0700

    multiple of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@
 def divisible_by_2(my_list=[]):
     list_result = []
     for i in my_list:
-        return lst_result.append(my_list[i] % 2 == 0)
+        return list_result.append(my_list[i] % 2 == 0)
 
     return (list_result)
