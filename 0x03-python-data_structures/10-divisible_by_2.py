db70c10f7202fe633d37cae3438232c207679d00
Omary Ally <omaryally.em@gmail.com>
2024-03-19 12:38:05 -0700

    multiple of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that finds all multiples of 2 in a list"""
+
+
+def divisible_by_2(my_list=[]):
+    list_result = []
+    for i in my_list:
+        return lst_result.append(my_list[i] % 2 == 0)
+
+    return (list_result)
