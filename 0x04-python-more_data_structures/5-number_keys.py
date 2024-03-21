4eab37bdee0a4771f79215a98bbaeebc4fbb720f
Omary Ally <omaryally.em@gmail.com>
2024-03-21 01:09:52 -0700

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that returns the number of keys in a dictionary"""
+
+
+def number_keys(a_dictionary):
+    a_keys = list(a_dictionary)
+
+    nb_keys = len(a_keys)
+
+    return nb_keys
