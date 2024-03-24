299502a1191c342df4a0308070d8ccfa05ba09c7
Omary Ally <omaryally.em@gmail.com>
2024-03-24 04:40:00 -0700

    Best score

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,8 +3,11 @@
 
 
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary.get(key) * 2
-
+    mult_dict = {}
     for key, value in a_dictionary.items():
+        mult_dict[key] = value * 2
+
+    for key, value in mult_dict.items():
         print(f"{key}: {value}")
+
+    return mult_dict
