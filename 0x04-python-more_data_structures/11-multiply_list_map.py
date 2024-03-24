6585c256129306d372fee1a27aef1652a42e2e4f
Omary Ally <omaryally.em@gmail.com>
2024-03-24 05:09:03 -0700

    multiply

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that returns a list with all values multiplied by a number"""
+
+
+def multiply_list_map(my_list=[], number=0):
+    from operator import mul
+    return list(map(lambda x: mul(x, number), my_list))
