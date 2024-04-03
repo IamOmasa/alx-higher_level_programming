f5b3d4d262fcf8a745a32b147650a02695d71d3d
Omary Ally <omaryally.em@gmail.com>
2024-04-03 12:17:36 -0700

    print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that prints an integer"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
+
