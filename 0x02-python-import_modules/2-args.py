eb3bb16e18a67538c8546893f7d3eb7b664d2a02
root <root@omasa>
2024-06-05 06:36:10 -0400

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+"""Print the number of and list of arguments."""
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments".format(count))
-    for i in range(count):
-        print("{}: {}\n".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+        import sys
+        count = len(sys.argv) - 1
+        if count == 0:
+            print("0 arguments.")
+        elif count == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments".format(count))
+        for arg in range(count):
+            print("{}: {}".format(arg + 1, sys.argv[arg + 1]))
