2fc95cc476ad4b7334252acd4a97fdf106df7d57
root <root@omasa>
2024-06-05 03:52:11 -0400

    add

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,2 +1,11 @@
 def add(a, b):
-    return (a + b)
+
+    result = 0
+
+    if a >= 0 and b >= 0:
+        result = a + b
+    elif a < 0 and b < 0:
+        result = -(abs(a) + abs(b))
+    else:
+        result = a + b
+    return result
