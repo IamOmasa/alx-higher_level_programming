5404776559580c337cdddecc1d73aec63d96e81e
root <root@omasa>
2024-06-07 10:33:08 -0400

    replace

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
-#!/usr/python3
+
 """function that retrieves an element from a list like in C"""
 
 
 def element_at(my_list, idx):
     my_len = len(my_list)
 
-    if idx < 0 or idx > (my_len - 1):
-        return (None)
-    else:
-        print(f"Element at index {idx} is {my_list[idx]}")
+    if idx < 0 or idx >= my_len:
+        return None
+    print(f"Element at index {idx} is {my_list[idx]}")
