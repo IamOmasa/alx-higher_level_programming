8fe8c6d538390047b8fac7bf0b987ab31fb64126
root <root@omasa>
2024-06-07 10:20:29 -0400

    retrieve

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,9 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return (None)
+    my_len = len(my_list)
 
-    return (my_list[idx])
+    if idx < 0 or idx > (my_len - 1):
+        return (None)
+    else:
+        print(f"Element at index {idx} is {my_list[idx]}")
