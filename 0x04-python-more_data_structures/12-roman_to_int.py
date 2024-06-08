b58bb21356b90baa153e9494380918e531ade87b
root <root@omasa>
2024-06-08 04:35:20 -0400

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" a function  that converts a Roman numeral to an integer"""
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_string_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+
+    total = 0
+    previous_value = 0
+
+    for char in roman_string:
+
+        current_value = roman_string_values.get(char, 0)
+
+        if current_value > previous_value:
+            total += current_value - 2 * previous_value
+        else:
+            total += current_value
+
+        previous_value = current_value
+
+    return total
