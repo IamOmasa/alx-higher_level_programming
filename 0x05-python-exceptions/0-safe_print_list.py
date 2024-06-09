35ae7bf84aa2b103531ac464c91e45a64752132b
root <root@omasa>
2024-06-09 03:13:37 -0400

    safe list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,11 +7,12 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count += 1
     except IndexError:
-        print("x is out of range")
-    finally:
-        print()
+        print(end="")
 
+    print ()
     return count
+
+    
