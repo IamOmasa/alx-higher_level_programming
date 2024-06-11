b2f8f40f2424de41e54e9a19844a774ff0828c7f
root <root@omasa>
2024-06-11 06:42:11 -0400

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""an empty class Square that defines a square"""
 
 
 class Square:
+    """an empty class Square that defines a square"""
     def __init__(self, height='0', width='0'):
         pass
