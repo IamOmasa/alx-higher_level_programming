9abb252ce8a4ee029e69e6f0b08d370a93d2d30e
root <root@omasa>
2024-06-11 07:28:15 -0400

    keep it private

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+"""a module that defines a class Square"""
 
 
 class Square:
+    """a class Square that defines a square """
     def __init__(self, size):
         """initilize a private attribute size with no type or value"""
         self.__size = size
