6d683441b8fd424aa5ace0f02b3e5632a2fdbe7c
root <root@omasa>
2024-06-11 06:29:20 -0400

    private

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,3 +6,9 @@ class Square:
     def __init__(self, size):
         """initilize a private instance size"""
         self.__size = size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
