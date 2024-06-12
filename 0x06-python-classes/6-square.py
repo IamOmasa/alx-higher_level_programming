128e2236a7f60223c6c2fbf14ca313ccf1a3bc12
root <root@omasa>
2024-06-12 16:31:17 -0400

    coordinate

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,7 +41,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @property
     def position(self):
         """Retrieve the value of position
